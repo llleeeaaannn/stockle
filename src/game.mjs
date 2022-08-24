237349dcb8a3db29536996d303380699c70ee75b
llleeeaaannn <piercefrank10@gmail.com>
2022-08-24 02:15:49 +0100

    Made game functional for 3 letter tickers

--- src/game.mjs
+++ src/game.mjs
@@ -2,12 +2,12 @@ import './style.css';
 import Day from './day.mjs';
 import Storage from './storage.mjs';
 import { keysArray, validLetters, colorKeys } from './variables.mjs';
-import { validAnswers, validWords } from './tickers.mjs';
+import { validAnswers, validTickers, validTickers2, validTickers3, validTickers4, validTickers5 } from './tickers.mjs';
 
 export default class Game {
 
   constructor() {
-    this.wordle = 'level';
+    this.wordle = 'AMD';
     this.gameWon = false;
     this.isGameOver = false;
     this.isHardModeOn = false;
@@ -20,12 +20,12 @@ export default class Game {
     this.hardModeOn = true;
     this.emojiCopyPaste = "";
     this.guesses = [
-      ['', '', '', '', ''],
-      ['', '', '', '', ''],
-      ['', '', '', '', ''],
-      ['', '', '', '', ''],
-      ['', '', '', '', ''],
-      ['', '', '', '', '']
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', '']
     ];
   }
 
@@ -34,6 +34,12 @@ export default class Game {
     this.makeTiles();
     this.makeKeyboardRows();
     this.makeKeyboardKeys();
+    this.makePopUp();
+  }
+
+  addListeners() {
+    this.keyPressListener();
+    this.scoreboardButtonListener();
   }
 
   // On page load, do the following to set variables as those stored locally:
@@ -86,7 +92,15 @@ export default class Game {
     });
   }
 
-  // Function to handle key on keyboard being clicked and call relevant function
+  // Function to create and insert the popup div
+  makePopUp() {
+    let popUpContainer = document.getElementById('tile-container')
+    let popUp = document.createElement('div');
+    popUp.setAttribute('id', 'popup');
+    popUpContainer.appendChild(popUp);
+  }
+
+  // Function to handle key onscreen keyboard being clicked
   click(letter) {
     if (!this.isGameOver.value) {
       if (letter === 'ENTER') {
@@ -99,11 +113,31 @@ export default class Game {
     }
   }
 
+  // Function to handle keyboard being clicked
+  keyPressed(event, that) {
+    let letter = event.key.toUpperCase();
+    if (validLetters.includes(letter)) {
+      that.click(letter);
+    } else if (letter == 'ENTER') {
+      this.isHardModeOn === 'On' ? this.checkGuessHard() : this.checkGuess();
+    } else if (letter == 'BACKSPACE') {
+      this.removeLetter();
+    }
+  }
+
+  // Function to add event listener to keyboard keydowns
+  keyPressListener() {
+    let that = this;
+    document.addEventListener('keydown', function() {
+      that.keyPressed(event, that);
+    });
+  }
+
   // Function to add a letter to the current tile & row
   addLetter(letter) {
     let currentTile = this.currentTile;
     let currentRow = this.currentRow;
-    if (currentRow < 6 && currentTile < 5) {
+    if (currentRow < 6 && currentTile < 3) {
       this.guesses[currentRow][currentTile] = letter;
       this.renderTile(letter, currentRow, currentTile)
       this.addToCurrentTile();
@@ -111,7 +145,7 @@ export default class Game {
   }
 
   removeLetter() {
-  let currentRow = this.currrentRow;
+  let currentRow = this.currentRow;
   let currentTile = this.currentTile;
   if (currentTile > 0) {
     this.removeFromCurrentTile();
@@ -154,44 +188,33 @@ export default class Game {
   renderTile(letter, row, tile) {
     let tileElement = document.getElementById('row' + row + 'tile' + tile);
     tileElement.textContent = letter;
-    tile.setAttribute('data', letter);
-    tile.classList.add('on-row');
+    tileElement.setAttribute('data', letter);
+    tileElement.classList.add('on-row');
   }
 
   renderEmptyTile(row, tile) {
     let tileElement = document.getElementById('row' + row + 'tile' + tile);
     tileElement.textContent = '';
-    tile.removeAttribute('data');
-    tile.classList.remove('on-row');
-  }
-
-  keyPressed(e) {
-    let letter = e.key.toUpperCase();
-    if (validLetters.includes(letter)) {
-      this.click(letter);
-    } else if (letter == 'ENTER') {
-      this.isHardModeOn === 'On' ? this.checkGuessHard() : this.checkGuess()
-    } else if (letter == 'BACKSPACE') {
-      this.removeLetter();
-    }
+    tileElement.removeAttribute('data');
+    tileElement.classList.remove('on-row');
   }
 
   checkGuess() {
     let currentRow = this.currentRow;
     let currentTile = this.currentTile;
-    let currentGuess = guesses[currentRow].join('').toLowerCase();
-    if (currentTile < 5) {
+    let currentGuess = this.guesses[currentRow].join('').toUpperCase();
+    if (currentTile < 3) {
       this.setPopUpMessage('Not enough letters');
-      this.invalidAnswerErrorDisplay();
-    } else if (validWords.includes(currentGuess) || validAnswers.includes(currentGuess) || currentGuess === wordle) {
-      if (currentTile === 5 && currentRow < 6) {
-        if (currentGuess === wordle) {
+      this.invalidAnswerDisplay();
+    } else if (validTickers.includes(currentGuess) || validAnswers.includes(currentGuess) || currentGuess === this.wordle) {
+      if (currentTile === 3 && currentRow < 6) {
+        if (currentGuess === this.wordle) {
           this.setGameOver(true);
           this.setGameOngoing(false);
           this.disableHardmodeCheckbox();
           this.gameWon = true;
           this.updateStatsOnWin();
-          this.setPopUpMessage('GIMME DAT');
+          this.setPopUpMessage('HUZZAH');
           this.colorTiles();
           this.jump();
           this.saveGuess();
@@ -227,15 +250,15 @@ export default class Game {
         }
       }
     } else {
-      this.setPopUpMessage('Not in word list');
-      this.invalidAnswerErrorDisplay();
+      this.setPopUpMessage('Not a ticker ');
+      this.invalidAnswerDisplay();
     }
   }
 
   // Function to color tiles and run function to color keys once answer is checked and then flip the row of tiles
   colorTiles() {
     let tiles = document.querySelector('#row' + this.currentRow).childNodes;
-    let checkWordle = wordle;
+    let checkWordle = this.wordle;
     let guess = [];
     let guessOuter = [];
     this.greenLetters = [];
@@ -246,7 +269,7 @@ export default class Game {
     })
 
     guess.forEach((guess, index) => {
-      let letter = guess.letter.toLowerCase();
+      let letter = guess.letter.toUpperCase();
       if (letter === this.wordle[index]) {
         guess.color = 'green-color';
         guess.num = 3;
@@ -256,10 +279,9 @@ export default class Game {
       }
     })
 
-
     guessOuter.forEach((outer, index) => {
       if (!(guess[index].color === 'green-color')) {
-        let letter = outer.letter.toLowerCase();
+        let letter = outer.letter.toUpperCase();
         if (checkWordle.includes(letter)) {
           guess[index].color = 'yellow-color';
           guess[index].num = 2;
@@ -268,7 +290,6 @@ export default class Game {
         }
       }
     })
-
 
     tiles.forEach((tile, index) => {
       tile.dataset.color = guess[index].color;
@@ -283,22 +304,27 @@ export default class Game {
       }, 1000 + 400 * index);
     })
 
-
     setTimeout(() => {
       this.colorEachKey(guess);
     }, 1700)
   }
 
-  //
+  // Function to color onscreen keys
   colorEachKey(guess) {
     guess.forEach((g) => {
       let key = document.getElementById(g.letter);
-      if (g.color === 'green-color') {
-        key.className = 'green-color-key';
-      } else if (g.color === 'yellow-color') {
-        key.className = 'yellow-color-key';
-      } else if (g.color === 'darkgrey-color') {
-        key.className = 'darkgrey-color-key';
+      if (key.className === 'green-color-key') {
+        return;
+      } else if (key.className === 'yellow-color-key') {
+        if (g.color === 'green-color') key.className = 'green-color-key';
+      } else {
+        if (g.color === 'green-color') {
+          key.className = 'green-color-key';
+        } else if (g.color === 'yellow-color') {
+          key.className = 'yellow-color-key';
+        } else if (g.color === 'darkgrey-color') {
+          key.className = 'darkgrey-color-key';
+        }
       }
     });
   }
@@ -347,9 +373,11 @@ export default class Game {
 
   // Code to populate previous guesses of that day upon page reload
   saveGuess() {
-    for (let tile = 0; tile < this.guesses[this.currentRow].length; tile++) {
-      let guess = this.guesses[this.currentRow][tile];
+    let currentRow = this.currentRow;
+    for (let tile = 0; tile < this.guesses[currentRow].length; tile++) {
+      let guess = this.guesses[currentRow][tile];
       let key = 'row' + currentRow + 'tile' + tile;
+      console.log(key);
       let color = document.getElementById(key).dataset.color;
       this.setGuessWithExpiry(key, guess, color);
     }
@@ -360,11 +388,19 @@ export default class Game {
   	let valueWithExpiry = {
   		value: value,
       color: color,
-      expiry: getNowZeroTime()
+      expiry: this.getNowZeroTime()
   	}
   	localStorage.setItem(key, JSON.stringify(valueWithExpiry))
   }
 
+  // Function to return the getTime of today at 00:00:00
+  getNowZeroTime() {
+    let now = new Date();
+    now.setHours(0);
+    now.setMinutes(0);
+    now.setSeconds(0, 0);
+    return now.getTime();
+  }
 
   // Function to call necessary functions when answer is not valid
   invalidAnswerDisplay() {
@@ -374,11 +410,13 @@ export default class Game {
 
   // Function to set popup message text
   setPopUpMessage(message) {
+    let popUpMessage = document.getElementById('popup');
     popUpMessage.innerHTML = `<p>${message}</p>`;
   }
 
   // Function to make pop up message to appear temporarily
   togglePopUp() {
+    let popUpMessage = document.getElementById('popup');
     popUpMessage.classList.toggle('popup-hide');
     setTimeout(() => {
       popUpMessage.classList.toggle('popup-hide');
@@ -387,6 +425,7 @@ export default class Game {
 
   // Function to make pop up message to appear temporarily but for longer
   togglePopUpLong() {
+    let popUpMessage = document.getElementById('popup');
     popUpMessage.classList.toggle('popup-hide');
     setTimeout(() => {
       popUpMessage.classList.toggle('popup-hide');
@@ -405,34 +444,30 @@ export default class Game {
   // Function to jump current row of tiles when guess is correct
   jump() {
 
-    function jumpArg(tile) {
-      let tileElement = document.getElementById('row' + this.currentRow + 'tile' + tile);
+    let currentRow = this.currentRow;
+
+    function jumpArg(tile, row) {
+      let tileElement = document.getElementById('row' + row + 'tile' + tile);
       tileElement.classList.toggle('jump');
     }
 
     setTimeout(() => {
-      jumpArg(0);
+      jumpArg(0, currentRow);
     }, 2000)
 
     setTimeout(() => {
-      jumpArg(1);
+      jumpArg(1, currentRow);
     }, 2300)
 
     setTimeout(() => {
-      jumpArg(2);
+      jumpArg(2, currentRow);
     }, 2600)
 
-    setTimeout(() => {
-      jumpArg(3);
-    }, 2900)
-
-    setTimeout(() => {
-      jumpArg(4);
-    }, 3200)
   }
 
   // Function to make pop up message to appear temporarily
   togglePopUp() {
+    let popUpMessage = document.getElementById('popup');
     popUpMessage.classList.toggle('popup-hide');
     setTimeout(() => {
       popUpMessage.classList.toggle('popup-hide');
@@ -441,6 +476,7 @@ export default class Game {
 
   // Function to make pop up message to appear temporarily but for longer
   togglePopUpLong() {
+    let popUpMessage = document.getElementById('popup');
     popUpMessage.classList.toggle('popup-hide');
     setTimeout(() => {
       popUpMessage.classList.toggle('popup-hide');
@@ -452,7 +488,7 @@ export default class Game {
     this.isGameOver = value;
     let storedIsGameOver = {
       value: value,
-      expiry: getNowZeroTime()
+      expiry: this.getNowZeroTime()
     }
     localStorage.setItem('GameOver', JSON.stringify(storedIsGameOver))
   }
@@ -462,13 +498,14 @@ export default class Game {
     this.isGameOngoing = value;
     let storedIsGameOngoing = {
       value: value,
-      expiry: getNowZeroTime()
+      expiry: this.getNowZeroTime()
     }
     localStorage.setItem('GameOngoing', JSON.stringify(storedIsGameOngoing))
   }
 
   // Function to diable Hard Mode checkbox if IsGameOngoing is true and the expiry is today
   disableHardmodeCheckbox() {
+    let hardModeCheckbox = document.getElementById('hard-mode-checkbox');
     this.isGameOngoing ? hardModeCheckbox.disabled = true : hardModeCheckbox.disabled = false;
   }
 
@@ -528,16 +565,110 @@ export default class Game {
     this.endStreak();
   }
 
+  // Function to toggle scoreboard
+  toggleLoadScoreboard() {
+    this.addScoreValues();
+    this.barChartLength();
+    let scoreboardContainer = document.getElementById('scoreboard-container');
+    let clockShareContainer = document.getElementById('clock-share-container');
+    scoreboardContainer.classList.toggle('scoreboard-hide');
+    if (this.isGameOver.value) {
+      clockShareContainer.classList.remove('hide-clock-share')
+    } else {
+      clockShareContainer.classList.add('hide-clock-share')
+    }
+  }
+
+  // Function to add event listener to scoreboard button
+  scoreboardButtonListener() {
+    let scoreboardButton = document.getElementById('scoreboard-button');
+    scoreboardButton.addEventListener('click', function() {
+      this.toggleLoadScoreboard();
+    });
+  }
+
+  // Code to populate scoreboard with current scores
+  addScoreValues() {
+    let playedValue = document.getElementById('played-value');
+    let winPercentageValue = document.getElementById('win-percentage-value');
+    let currentStreakValue = document.getElementById('current-streak-value');
+    let maxStreakValue = document.getElementById('max-streak-value');
+    let played = Number(localStorage.getItem('TotalGames'));
+    let wins = Number(localStorage.getItem('TotalWins'));
+    let winPercentage =  Math.floor(wins / played * 100);
+    let currentStreak = Number(localStorage.getItem('CurrentStreak'));
+    let maxStreak = Number(localStorage.getItem('MaxStreak'));
+    playedValue.textContent = `${played}`;
+    winPercentageValue.textContent = `${winPercentage}`;
+    currentStreakValue.textContent = `${currentStreak}`;
+    maxStreakValue.textContent = `${maxStreak}`;
+  }
+
+  // Code to define and calculate bar chart bar lengths
+  barChartLength() {
+    let barChartOne = Number(localStorage.getItem('Row1Wins'));
+    let barChartTwo = Number(localStorage.getItem('Row2Wins'));
+    let barChartThree = Number(localStorage.getItem('Row3Wins'));
+    let barChartFour = Number(localStorage.getItem('Row4Wins'));
+    let barChartFive = Number(localStorage.getItem('Row5Wins'));
+    let barChartSix = Number(localStorage.getItem('Row6Wins'));
+
+    let barCharts = [barChartOne, barChartTwo, barChartThree, barChartFour, barChartFive, barChartSix];
+    let maxBar = Math.max.apply(Math.max, barCharts);
+
+    let barChartOneLength = Math.floor(barChartOne / maxBar * 100);
+    let barChartTwoLength = Math.floor(barChartTwo / maxBar * 100);
+    let barChartThreeLength = Math.floor(barChartThree / maxBar * 100);
+    let barChartFourLength = Math.floor(barChartFour / maxBar * 100);
+    let barChartFiveLength = Math.floor(barChartFive / maxBar * 100);
+    let barChartSixLength = Math.floor(barChartSix / maxBar * 100);
+
+    let barOne = document.getElementById('bar-chart-one');
+    barOne.style.width = `${barChartOneLength}%`
+    barOne.innerHTML = `<p>${barChartOne}</p>`
+
+    let barTwo = document.getElementById('bar-chart-two');
+    barTwo.style.width = `${barChartTwoLength}%`
+    barTwo.innerHTML = `<p>${barChartTwo}</p>`
+
+    let barThree = document.getElementById('bar-chart-three');
+    barThree.style.width = `${barChartThreeLength}%`
+    barThree.innerHTML = `<p>${barChartThree}</p>`
+
+    let barFour = document.getElementById('bar-chart-four');
+    barFour.style.width = `${barChartFourLength}%`
+    barFour.innerHTML = `<p>${barChartFour}</p>`
+
+    let barFive = document.getElementById('bar-chart-five');
+    barFive.style.width = `${barChartFiveLength}%`
+    barFive.innerHTML = `<p>${barChartFive}</p>`
+
+    let barSix = document.getElementById('bar-chart-six');
+    barSix.style.width = `${barChartSixLength}%`
+    barSix.innerHTML = `<p>${barChartSix}</p>`
+  }
+
+  // Code to define which day it is
+  wordleNumber() {
+    let today = new Date();
+    let milliSince1970 = today.getTime();
+    let milliPerDay = 86400000;
+    let daysSince1970 = Math.floor(milliSince1970/milliPerDay);
+    let dailyDate = daysSince1970 - 19227 + 0;
+    console.log(dailyDate);
+    return dailyDate;
+  }
+
   // Code to copy the results of the users daily wordle to the clipboard upon them clicking on the Share button in thr scoreboard
   copyResults() {
     if (this.gameWon === true) {
-      if (this.isHardModeOn()) {
+      if (this.isHardModeOn) {
         this.emojiCopyPaste += `Wordle ${this.wordleNumber()} ${this.currentRow + 1}/6*\n`
       } else {
         this.emojiCopyPaste += `Wordle ${this.wordleNumber()} ${this.currentRow + 1}/6\n`
       }
     } else {
-      if (this.isHardModeOn()) {
+      if (this.isHardModeOn) {
         this.emojiCopyPaste += `Wordle ${this.wordleNumber()} X/6*\n`
       } else {
         this.emojiCopyPaste += `Wordle ${this.wordleNumber()} X/6\n`
@@ -545,8 +676,8 @@ export default class Game {
     }
 
     for (let i = 0; i <= this.currentRow; i++) {
-      let thisGuessRow = guesses[i];
-      let checkWordle = wordle;
+      let thisGuessRow = this.guesses[i];
+      let checkWordle = this.wordle;
       let guess = [];
       let guessOuter = [];
       let guessEmojiColors = [];
@@ -557,8 +688,8 @@ export default class Game {
       })
 
       guess.forEach((guessLetter, index) => {
-          thisLetter = guessLetter.letter.toLowerCase();
-          if (thisLetter === wordle[index]) {
+          let thisLetter = guessLetter.letter.toLowerCase();
+          if (thisLetter === this.wordle[index]) {
               guessLetter.color = 'green';
               checkWordle = checkWordle.replace(thisLetter, '');
               guessOuter[index] = ' ';
@@ -567,7 +698,7 @@ export default class Game {
 
       guessOuter.forEach((outer, index) => {
         if (!(guess[index].color === 'green')) {
-          thisLetter = outer.letter.toLowerCase();
+          let thisLetter = outer.letter.toLowerCase();
           if (checkWordle.includes(thisLetter)) {
             guess[index].color = 'yellow';
             checkWordle = checkWordle.replace(thisLetter, '');
@@ -575,7 +706,7 @@ export default class Game {
         }
       })
 
-      for (x = 0; x < guess.length; x++) {
+      for (let x = 0; x < guess.length; x++) {
         if (guess[x].color === 'green') {
           this.emojiCopyPaste += String.fromCodePoint(0x1F7E9);
         } else if (guess[x].color === 'yellow') {
--- src/script.js
+++ src/script.js
@@ -1,9 +1,10 @@
 import './style.css';
 import Day from './day.mjs';
 import Game from './game.mjs';
-import Tickers from './tickers.mjs';
+import { validTickers, validAnswers } from './tickers.mjs';
 import Storage from './storage.mjs';
 import { keysArray } from './variables.mjs';
 
 let game = new Game();
 game.createUI();
+game.addListeners();
