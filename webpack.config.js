c87587f561de21a63a41d4274d13db815b78fcbb
llleeeaaannn <piercefrank10@gmail.com>
2022-09-02 15:53:25 +0100

    Added favicon and updated code

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,14 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       }
