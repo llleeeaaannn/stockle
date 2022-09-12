033c00ed7691f0ec07bd893a0decdf17cb57fad3
llleeeaaannn <piercefrank10@gmail.com>
2022-09-12 13:38:07 +0100

    Added orientation message

--- dist/8d843ee15eaa358d45fb.js
+++
@@ -1,1 +0,0 @@
-import"./style.css";import Game from"./game.mjs";const oppositeOrientation=screen.orientation.type;console.log(oppositeOrientation),screen.orientation.lock(oppositeOrientation);const game=new Game;game.play();
--- src/script.js
+++ src/script.js
@@ -1,9 +1,5 @@
 import './style.css';
 import Game from './game.mjs';
 
-const oppositeOrientation = screen.orientation.type.startsWith('portrait') ? 'portrait' : 'landscape';
-console.log(oppositeOrientation);
-screen.orientation.lock(oppositeOrientation);
-
 const game = new Game();
 game.play();
